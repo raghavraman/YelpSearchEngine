e14103b6efa2f84b08e63941606cf764c758ad15
RaghavRaman <rghv.raman@gmail.com>
2019-04-18 19:55:44 -0400

    Added first level sorting

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import {connect} from 'react-redux';
 import {yelpAPI} from './apis/searchApi';
 import TextFieldGroup from './components/TextFieldGroup';
 import ReviewTable from './components/ReviewTable';
-import Review from './components/Review';
 import PropTypes from 'prop-types';
 class Main extends Component {
   constructor(){
@@ -13,6 +12,7 @@ class Main extends Component {
     };
     this.onChange= this.onChange.bind(this);
     this.onSubmit= this.onSubmit.bind(this);
+    this.sortResults= this.sortResults.bind(this);
   }
 
   onSubmit(e){
@@ -26,16 +26,28 @@ class Main extends Component {
     this.setState({[e.target.name]: e.target.value});
   }
 
+  sortResults(){
+    const  {yelpResults,loading} =this.props.search;
+    let res =  yelpResults.sort(this.sortDescending("ratings"))
+    this.setState({yelpResults: res});
+  }
+
+  sortDescending(property) {
+    console.log(property);
+
+    return function (a,b) {
+        return (a[property] > b[property]) ? -1 : (a[property] < b[property]) ? 1 : 0;
+    }
+  }
 
   render() {
     const  {yelpResults,loading} =this.props.search;
     let display = "";
     if(loading){
-      console.log('reached if');
       display = <p className="lead text-center">"No results found"</p>;
     }
     else{
-      console.log('reached else');
+      console.log("raghav")
       display = <ReviewTable reviews = {yelpResults} />;
 
     }    
@@ -46,15 +58,15 @@ class Main extends Component {
             <div className="col-md-10 md-auto">        
               <TextFieldGroup name="searchkey" placeholder="Search Yelp" type="text" value={this.state.searchkey} onChange={this.onChange} />
             </div>
-          <div className="col-md-2 md-auto">        
-            <input type="submit" className="btn btn-primary btn-lg" value="Search" />
+            <div className="col-md-2 md-auto">        
+              <input type="submit" className="btn btn-primary btn-lg" value="Search" />
+                <img className="arrow" onClick = { this.sortResults}
+                         src="https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-arrow-down-b-128.png"/>
+            </div>
           </div>
-        </div>
-      </form>
-      <div className="row">
-      {display}
-      </div>
-</div>  
+        </form>
+        {display}
+      </div>  
     );
   }
 }
