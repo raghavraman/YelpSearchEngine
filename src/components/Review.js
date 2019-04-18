3037fc041d450992a134ac9256fe231262fbb793
Lazarus1993 <ashutoshburde@gmail.com>
2019-04-18 19:54:13 -0400

    CSS stuff

--- src/components/Review.js
+++ src/components/Review.js
@@ -1,22 +1,38 @@
-import React, { Component } from 'react';
-import classnames from 'classnames';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import classnames from "classnames";
+import PropTypes from "prop-types";
 
 class Review extends Component {
-	render(){
-		const {review} = this.props;
-
-		return (<div>
-			<p>THe buisness is: {review.businessName}</p>
-			<p>THe Rating is: {review.ratings}</p>
-			<p>THe Review is: {review.text}</p>
-			</div>
-		);
-	}
-};
+  render() {
+    const { review } = this.props;
+    let stars = [];
+    let value = parseInt(review.ratings);
+    console.log(value);
+    for (let j = 0; j < value; j++) {
+      stars.push(<i className="fas fa-star" />);
+    }
+    console.log(review.ratings);
+    if (value !== review.ratings) {
+      stars.push(<i className="fas  fa-star-half" />);
+    }
+    return (
+      <div className="card card-body mb-3">
+        <div className="row">
+          <div className="col-md-10">
+            <h3>{review.businessName}</h3>
+            <p>{review.text}</p>
+          </div>
+          <div className="col-md-2">
+            <div>{stars}</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 Review.propTypes = {
-	review:PropTypes.object.isRequired
+  review: PropTypes.object.isRequired
 };
 
 export default Review;
